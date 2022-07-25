1b04c383d93bcb382d8c7c4599c35d30368e6aa0
Henrylesatele <henry.lesatele@gmail.com>
2022-07-25 09:44:03 -0400

    see

--- Henry/GC/GC.gd
+++ Henry/GC/GC.gd
@@ -4,7 +4,6 @@ func _on_GC_ready():
 	pass
 
 func _input(event):
-	print (len(get_overlapping_bodies()) > 1)
 	if event.is_action_pressed("game_usage") and len(get_overlapping_bodies()) > 1:
 		find_and_use_dialogue()
 func find_and_use_dialogue():
@@ -15,7 +14,6 @@ func _on_GC_body_exited(_body):
 	var dialogue_playerI = get_node_or_null("Dialogueplayer I")
 	if dialogue_playerI:
 		dialogue_playerI.reset()
-	print("left")
 func _process(_delta):
 	if len(get_overlapping_bodies()) == 0:
 		var dialogue_playerI = get_node_or_null("Dialogueplayer I")
--- Henry/Laura/Laura.gd
+++ Henry/Laura/Laura.gd
@@ -4,7 +4,6 @@ func _on_Laura_ready():
 	pass
 
 func _input(event):
-	print (len(get_overlapping_bodies()) > 1)
 	if event.is_action_pressed("game_usage") and len(get_overlapping_bodies()) > 1:
 		find_and_use_dialogue()
 func find_and_use_dialogue():
@@ -15,7 +14,6 @@ func _on_GC_body_exited(_body):
 	var dialogue_playerII = get_node_or_null("Dialogueplayer II")
 	if dialogue_playerII:
 		dialogue_playerII.reset()
-	print("left")
 func _process(_delta):
 	if len(get_overlapping_bodies()) == 0:
 		var dialogue_playerII = get_node_or_null("Dialogueplayer II")
