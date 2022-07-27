f6b06debb5a157a780fcd31d46a261ee1e082594
IFP A <IFPA@CPL-IFPMBP07.local>
2022-07-27 11:01:55 -0400

    Added door collision

--- scenes/Hall/Transition_Object3.gd
+++ scenes/Hall/Transition_Object3.gd
@@ -29,3 +29,4 @@ func _on_Area_input_event(camera, event, position, normal, shape_idx):
 
 func _on_Timer_timeout():
 	get_tree().change_scene("res://CreationScene.tscn")
+#switch to a different game
