1ac5980c5cdfc624bf71b9e5b7cfcd7bafed4a21
IFP A <IFPA@CPLs-MacBook-Pro.local>
2022-08-01 10:57:01 -0400

    Hallway plus uri's game plus henry's dialogues, together at LAST

---
+++ scenes/Charater scenes/Student/StudentII.gd
@@ -0,0 +1,43 @@
+extends Area
+
+var target = 0
+var velocity = Vector3(0,0,0)
+var speed = 0.2
+var path = []
+
+var dialogue_player = null
+
+func _input(event):
+	if event.is_action_pressed("game_usage") and len(get_overlapping_bodies()) > 1:
+		find_and_use_dialogue()
+func find_and_use_dialogue():
+	dialogue_player = get_node_or_null("Dialogueplayer")
+	if dialogue_player:
+		if not dialogue_player.next_line():
+			print("now changing target")
+			target = 1
+			
+func _on_Mentor_body_exited(_body):
+	dialogue_player = get_node_or_null("Dialogueplayer")
+	if dialogue_player:
+		dialogue_player.reset()
+func _process(_delta):
+	if len(get_overlapping_bodies()) == 0:
+		dialogue_player = get_node_or_null("Dialogueplayer")
+		if dialogue_player:
+			dialogue_player.reset()
+
+func _physics_process(_delta):
+	if target == 0 and path.size() == 0:
+		path = [Vector3(-20,1,-29),Vector3(3,1,-43),Vector3(-11,1,-32),Vector3(10,1,-43)]
+	if path.size() > 0:
+		velocity = Vector3(path[0].x - transform.origin.x, path[0].y - transform.origin.y, path[0].z - transform.origin.z)
+		var distance =  velocity.length()
+		print(distance)
+		if distance <= speed:
+			target = 1 
+			path = path.slice(1,path.size())
+			velocity = Vector3(0,0,0)
+		else:
+			velocity = velocity.normalized() * speed
+	transform.origin += velocity
---
+++ scenes/Main Componet scenes/School/Enviorment scene/Area2D.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('Right'):
+		velocity.x += speed
+	if Input.is_action_pressed('Left'):
+		velocity.x -= speed
+	if Input.is_action_pressed('Down'):
+		velocity.y += speed
+	if Input.is_action_pressed('Up'):
+		velocity.y -= speed
