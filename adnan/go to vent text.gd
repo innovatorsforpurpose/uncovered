f6ea3508a6498564407f2d2f2d3796341a9bb5cd
adnan <adnanmek06@gmail.com>
2023-03-22 16:45:40 -0400

    Most of basement in this commit

---
+++ adnan/go to vent text.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+# Declare member variables here. Examples:
+export var paused = true# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+	pass # Replace with function body.
+
+
+
+#func _process(delta):
+#	pass
+
+
+func _on_Area2_body_entered(body):
+	if body.name == "MC":
+		get_node("/root/RootScene/BASEMENT/go to vent text").visible = true
+		get_node("/root/RootScene/BASEMENT/go to vent text/Timer").start(3)
+	
+	pass # Replace with function body.
+
+
+func _on_Timer_timeout():
+	visible = false
+	pass # Replace with function body.
