190af6a60a22aae91f380b13118b2f99d78acfb9
adnan <adnanmek06@gmail.com>
2023-04-05 17:57:59 -0400

    Updated on conversation, and made improvements

--- adnan/Conversation.gd
+++ adnan/Conversation.gd
@@ -11,20 +11,29 @@ func _ready():
 	visible = false
 	pass # Replace with function body.
 
-
+var counter = 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var counter = 1
-	if Input.is_action_pressed("game_usage"):
+	
+	if Input.is_action_just_pressed("game_usage") and counter < 8:
+		print (counter)
 		get_node("/root/RootScene/BASEMENT/Converstaion/Node2D/Instructions/MarginContainer/HBoxContainer/Label" + str(counter)).visible = false
 		get_node("/root/RootScene/BASEMENT/Converstaion/Node2D/Instructions/MarginContainer/HBoxContainer/Label" + str(counter + 1)).visible = true
 		counter += 1
+	elif Input.is_action_just_pressed("game_usage"):
+		dialogue_done = true		
+		
+		
+		
 		
 		
 	pass
 
-
+var dialogue_done = false
 func _on_Area3_body_entered(body):
-	if body.name == "MC":
+	if body.name == "MC" and !dialogue_done:
 		get_node("/root/RootScene/BASEMENT/Converstaion").visible = true
+	elif dialogue_done:
+		get_node("/root/RootScene/BASEMENT/Converstaion").visible = true
+				
 	pass # Replace with function body.
