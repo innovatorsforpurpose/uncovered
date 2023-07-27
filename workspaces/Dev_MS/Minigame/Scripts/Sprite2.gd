4ee5976b1cb1d61312f031612758631c66c32204
Henrylesatele <henry.lesatele@gmail.com>
2023-07-27 11:23:49 -0400

    falling floor animation fixed

--- workspaces/Dev_MS/Minigame/Scripts/Sprite2.gd
+++ workspaces/Dev_MS/Minigame/Scripts/Sprite2.gd
@@ -1,14 +1,17 @@
 extends Sprite
 
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
 
 
-
-func _on_red_area_body_entered(body):
-	visible = true
-
-
-func _on_red_area_body_exited(body):
-	visible = false 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
