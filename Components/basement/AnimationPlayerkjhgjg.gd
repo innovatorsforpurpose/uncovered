8b0f63a932ec3d810d8112a7372f319ba80b945f
Henrylesatele <henry.lesatele@gmail.com>
2023-07-27 10:23:45 -0400

    moved files To Basement / Hallway 1

---
+++ Components/basement/AnimationPlayerkjhgjg.gd
@@ -0,0 +1,24 @@
+extends AnimationPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#play("open")
+	pass # Replace with function body.
+
+var animation_played = false
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_body_shape_entered(_body_rid, body, _body_shape_index, _local_shape_index):
+	if body.name == "MC" and animation_played == false:	
+		get_node('.').play("open")
+		animation_played = true 
+	pass # Replace with function body.
