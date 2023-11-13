8fc84c6032fec6c2615e7ce83c534479137fddc4
Henrylesatele <henry.lesatele@gmail.com>
2023-11-13 16:00:39 -0500

    All dots now function accordingly and corrosponds with each colored line

--- minigames/bluedrag.gd
+++ minigames/bluedrag.gd
@@ -3,16 +3,15 @@ extends KinematicBody2D
 var dragReady = false
 var myClickPos 
 var myDistance = Vector2(0,0)
+var completed = false
 
 
 func _physics_process(_delta):
-	
-	
 	if Input.is_action_pressed("click") and dragReady:
 		myClickPos = get_global_mouse_position()
 		#myDistance = get_position()- myClickPos
 		#print(myDistance)
-	if Input.is_action_pressed("click") and dragReady:
+	if Input.is_action_pressed("click") and dragReady and not completed:
 		set_position(get_global_mouse_position())
 
 func _on_KinematicBody2D_mouse_entered():
@@ -24,9 +23,8 @@ func _on_KinematicBody2D_mouse_exited():
 	dragReady = false
 	get_node("../..").bluedrag = false
 
-func _on_Area2D_body_entered(_body):
-	
-	print("hello")
-
-func _on_reddrag_tree_entered():
-	dragReady = false
+func _on_Blue_area_body_entered(body):
+	# This makes the blue dot drop when in correct area
+	if Input.is_action_pressed("click") and body.name == "bluedrag":
+		completed = true
+		dragReady = false
--- minigames/yellowdrag.gd
+++ minigames/yellowdrag.gd
@@ -3,16 +3,14 @@ extends KinematicBody2D
 var dragReady = false
 var myClickPos 
 var myDistance = Vector2(0,0)
-
+var completed = false
 
 func _physics_process(_delta):
-	
-	
 	if Input.is_action_pressed("click") and dragReady:
 		myClickPos = get_global_mouse_position()
 		#myDistance = get_position()- myClickPos
 		#print(myDistance)
-	if Input.is_action_pressed("click") and dragReady:
+	if Input.is_action_pressed("click") and dragReady and not completed:
 		set_position(get_global_mouse_position())
 
 func _on_KinematicBody2D_mouse_entered():
@@ -27,5 +25,11 @@ func _on_KinematicBody2D_mouse_exited():
 func _on_Area2D_body_entered(_body):
 	print("hello")
 
-func _on_reddrag_tree_entered():
-	pass
+
+
+
+func _on_yellow_area_body_entered(body):
+	# This makes the yellow dot drop when in correct area
+	if Input.is_action_pressed("click") and body.name == "yellowdrag":
+		completed = true
+		dragReady = false
