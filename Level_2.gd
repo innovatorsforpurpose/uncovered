12dfaa3eb9158aed07435fa2f734272abe567611
Henrylesatele <henry.lesatele@gmail.com>
2024-02-01 19:14:06 -0500

    finalized text software which has been simplified and streamlined for better universal implications - currently working on getting the location of Level one message / function of said message.

--- Level_2.gd
+++ Level_2.gd
@@ -1,31 +1,36 @@
 extends Control
-onready var Px = $Panel
-onready var Lx = $Panel/Label
-onready var IDx = $Panel/NinePatchRect
-onready var TQx = $Panel/NinePatchRect2
-onready var NQx = $Panel/NinePatchRect2/Label
-onready var IQx = $Panel/Backtext
-
-var active_dialogue = 0
-
-func _ready() -> void:
-	pass
-
-func _physics_process(delta):
-	if Input.is_action_just_pressed("game_usage"):
-		active_dialogue = active_dialogue+1
-		visible = true
-	if active_dialogue == 0:
-		Lx.hide()
-		NQx.hide()
-	if active_dialogue == 1:
-		Lx.text = "T1"
-		NQx.text = "you"
-	if active_dialogue == 2:
-		Lx.text = "T2"
-		NQx.text = "Zach"
-	if active_dialogue == 3:
-		Lx.hide()
-		NQx.hide()
-		
-		visible = false
+#onready var Px = $Panel
+#onready var Lx = $Panel/Label
+#onready var IDx = $Panel/NinePatchRect
+#onready var TQx = $Panel/NinePatchRect2
+#onready var NQx = $Panel/NinePatchRect2/Label
+#onready var IQx = $Panel/Backtext
+#
+#var active_dialogue = 0
+#
+#func _ready() -> void:
+#	pass
+#
+#func _physics_process(delta):
+#	if  active_dialogue == 0 and Input.is_action_just_pressed("game_usage") == false:
+#		visible = false
+#
+#	if Input.is_action_just_pressed("game_usage") == true:
+#		active_dialogue = active_dialogue+1
+#		visible = true
+#
+#	if active_dialogue == 1:
+#		Lx.text = "T1"
+#		NQx.text = "you"
+#		IQx.show()
+#		Px.show()
+#		IDx.show()
+#		TQx.show()
+#
+#	if active_dialogue == 2:
+#		Lx.text = "T2"
+#		NQx.text = "Zach"
+#		IQx.show()
+#		Px.show()
+#		IDx.show()
+#		TQx.show()
